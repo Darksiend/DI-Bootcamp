f5935aef397b05a6f397c0ab312a170fafa7dcea
Darksiend <logosmk2@gmail.com>
2022-10-11 18:11:19 +0300

    Ex1 and Ex2 Done

---
+++ Week2/Day1/ExercisesXP/script.js
@@ -0,0 +1,17 @@
+// Ex1
+let favFood = "Burger"
+let favMeal = "Dinner"
+
+console.log(`I eat ${favFood} at every ${favMeal}`)
+
+//Ex2
+
+let myWatchedSeries = ["black mirror", "money heist", "the big bang theory"]
+
+let myWatchedSeriesLength = myWatchedSeries.length
+
+let myWatchedSeriesSentence = myWatchedSeries[0] + myWatchedSeries[2]
+
+console.log(
+  `I watched ${myWatchedSeriesLength} series : ${myWatchedSeriesSentence}`
+)
