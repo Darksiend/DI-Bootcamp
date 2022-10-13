836b5d31b007e77277de556319d0bde39ff1754e
darksiend <logosmk2@icloud.com>
2022-10-13 16:06:58 +0300

    Done Ex4

--- Week2/Day1/ExercisesXP/script.js
+++ Week2/Day1/ExercisesXP/script.js
@@ -20,3 +20,23 @@ console.log(
 
 let tempCel = 36.6
 console.log(`${tempCel}°C is ${(tempCel / 5) * 9 + 32}`)
+
+//Ex4
+
+let c
+let a = 34
+let b = 21
+
+console.log(a + b) //first expression
+// Prediction: a = 34 and b = 21 so it will be 55
+// Actual: 55
+
+a = 2
+
+console.log(a + b) //second expression
+// Prediction: a = 2 b = 21 it will be 23
+// Actual:23
+// Value of c is undefind
+
+console.log(3 + 4 + "5")
+// Here iutcome will be string 75
