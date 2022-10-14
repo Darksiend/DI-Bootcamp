2bda118532ce7ef1370396ab643e4f0814dcc8f1
Darksiend <logosmk2@gmail.com>
2022-10-14 10:58:42 +0300

    Ex 3 Done Gold

--- Week2/Day1/ExercisesXPGold/script.js
+++ Week2/Day1/ExercisesXPGold/script.js
@@ -21,3 +21,30 @@ str2 = arr2.join()
 str3 = str1 + " " + str2
 
 console.log(str3)
+
+//Ex3
+
+function readNumber() {
+  do {
+    let num = prompt("Put Here a number!", 0)
+    res = Number(num)
+  } while (isNaN(res))
+  return res
+}
+
+num1 = readNumber()
+num2 = readNumber()
+
+function sum(num1, num2) {
+  return num1 + num2
+}
+
+function div(num1, num2) {
+  return num1 / num2
+}
+
+function multipy(num1, num2) {
+  return num1 * num2
+}
+
+alert(sum(num1, num2))
