a55996be27a07a9b51214b7e33f6a60d12cc82b2
Darksiend <logosmk2@gmail.com>
2022-10-14 10:27:15 +0300

    Done Ex2 Gold

--- Week2/Day1/ExercisesXPGold/script.js
+++ Week2/Day1/ExercisesXPGold/script.js
@@ -4,3 +4,20 @@ let sentence = []
 sentence.push("my", "favorite", "color", "is", "blue")
 
 console.log(sentence.join(" "))
+
+//Ex2
+
+let str1 = "bamba"
+let str2 = "cola"
+
+arr1 = str1.split("")
+arr2 = str2.split("")
+arr1[0] = str2[0]
+arr2[0] = str1[0]
+
+str1 = arr1.join()
+str2 = arr2.join()
+
+str3 = str1 + " " + str2
+
+console.log(str3)
