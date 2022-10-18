5f79de8774dfc64c2412a3cd03d19338fd0270c7
Darksiend <logosmk2@gmail.com>
2022-10-18 12:42:12 +0300

    Ex 2 Work

--- Week2/Day2/ExercisesXPNinja/script.js
+++ Week2/Day2/ExercisesXPNinja/script.js
@@ -1,8 +1,23 @@
 //Ex1
 
-const firsYear = 1999
-const secondYear = 1990
+const firsYear = 1999;
+const secondYear = 1990;
 
-date = secondYear + (firsYear - secondYear) * 2
+date = secondYear + (firsYear - secondYear) * 2;
 
-console.log(date)
+console.log(date);
+
+//Ex2
+
+let zip = "56734";
+
+if (zip.length === 5) {
+  let allNumbers = true;
+  for (i = 0; i < zip.length && allNumbers == true; i++) {
+    if (zip[i] >= "0" && zip[i] <= "9") {
+      allNumbers = true;
+    } else {
+      console.log("Error!");
+    }
+  }
+}
