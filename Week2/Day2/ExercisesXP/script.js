2fc9796bcc91d70111b748a8b62bf7043021db6a
Darksiend <logosmk2@gmail.com>
2022-10-18 10:32:33 +0300

    Ex4 Done

--- Week2/Day2/ExercisesXP/script.js
+++ Week2/Day2/ExercisesXP/script.js
@@ -31,3 +31,25 @@ if (number % 2 === 0) {
 } else {
   console.log(`${number} is an odd number`)
 }
+
+//Ex4
+
+const users = [
+  "Lea123",
+  "Princess45",
+  "cat&doglovers",
+  "helooo@000",
+  "Darksiend",
+]
+
+if (users.length === 0) {
+  console.log("no one is online")
+} else if (users.length === 1) {
+  console.log(`${users[0]} is online`)
+} else if (users.length === 2) {
+  console.log(`${users.slice(0, 2).join(",")} is online`)
+} else if (users.length > 2) {
+  console.log(
+    `${users.slice(0, 2).join(",")} and ${users.length - 2} is online`
+  )
+}
