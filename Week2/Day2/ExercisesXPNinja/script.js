33ca8aa099a4b3276bf9040544621e3522c488d2
Darksiend <logosmk2@gmail.com>
2022-10-18 13:30:45 +0300

    Ex3 Done

--- Week2/Day2/ExercisesXPNinja/script.js
+++ Week2/Day2/ExercisesXPNinja/script.js
@@ -27,3 +27,27 @@ console.log(/^[0-9]+$/.test(zip));
 if (/^[0-9]+$/.test(zip)) {
   console.log("Sucsess");
 }
+
+//Ex3
+
+word = "aebhukldn";
+
+console.log(
+  word.replace(/[aeiouy]/gi, function (char) {
+    if (char == "a") {
+      return 1;
+    }
+    if (char == "e") {
+      return 2;
+    }
+    if (char == "i") {
+      return 3;
+    }
+    if (char == "o") {
+      return 4;
+    }
+    if (char == "u") {
+      return 5;
+    }
+  })
+);
