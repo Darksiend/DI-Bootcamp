e5eda5bd9fcbf58223f36595e6a6ace2b502d299
Darksiend <logosmk2@gmail.com>
2022-10-18 10:14:35 +0300

    Ex2 Done

---
+++ Week2/Day2/ExercisesXP/script.js
@@ -0,0 +1,23 @@
+//Ex1
+
+let x = 5
+let y = 2
+
+if (x > y) {
+  console.log(`${x} is the biggest number`)
+} else {
+  console.log(`${y} is the biggest number`)
+}
+
+//Ex2
+
+let newDog = "Chihuahua"
+console.log(`${newDog} have ${newDog.length} letters`)
+console.log(newDog.toUpperCase())
+console.log(newDog.toLowerCase())
+
+if (newDog === "Chihuahua") {
+  console.log("I love Chihuahuas, its my favorite dog breed")
+} else {
+  console.log("I dont care, I prefer cats")
+}
