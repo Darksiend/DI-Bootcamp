8e691fd4f57deb6662fa90f1b93280db7a9eefb3
Darksiend <logosmk2@gmail.com>
2022-10-19 20:28:29 +0300

    Work

--- Week2/Day3/DailyChallenge/script.js
+++ Week2/Day3/DailyChallenge/script.js
@@ -8,10 +8,10 @@ for (i = 0; i < 6; i++) {
 
 //With 2 Loops
 let str = "";
-for (i = 0; i < 5; i++) {
-  for (i = 0; i < 5; i++) {
+for (i = 0; i < 7; i++) {
+  for (j = 0; j < i; j++) {
     str += "*";
-    console.log(str);
   }
-  print("\n");
+  console.log(str);
+  str = "";
 }
