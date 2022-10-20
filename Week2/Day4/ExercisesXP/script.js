d0936d1e15a6798624cd2f8b1c159e9074ca60ef
Darksiend <logosmk2@gmail.com>
2022-10-20 12:12:59 +0300

    Done Calculator of Tips

--- Week2/Day4/ExercisesXP/script.js
+++ Week2/Day4/ExercisesXP/script.js
@@ -14,3 +14,32 @@ function infoAboutPerson(personName, personAge, personFavoriteColor) {
 infoAboutPerson("Anton", 23, "blue");
 infoAboutPerson("David", 45, "blue");
 infoAboutPerson("Josh", 12, "yellow");
+
+//Ex2
+
+function calculateTip() {
+  let bill = prompt("Bill");
+  if (!isNaN(bill)) {
+    if (bill < 50) {
+      console.log(
+        `Your tip: ${(bill * 0.2).toFixed(2)} and your bill is ${(
+          bill * 1.2
+        ).toFixed(2)}`
+      );
+    } else if (bill > 50 && bill < 200) {
+      console.log(
+        `Your tip: ${(bill * 0.15).toFixed()} and your bill is ${(
+          bill * 1.15
+        ).toFixed(2)}`
+      );
+    } else if (bill > 200) {
+      console.log(
+        `Your tip: ${(bill * 0.1).toFixed(2)} and your bill is ${(
+          bill * 1.1
+        ).toFixed(2)}`
+      );
+    }
+  }
+}
+
+calculateTip();
