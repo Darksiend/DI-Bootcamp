745f42cbf45b238b748ca55ef8fed05717e07397
Darksiend <logosmk2@gmail.com>
2022-10-20 11:46:49 +0300

    Done

--- Week2/Day3/DailyChallenge/script.js
+++ Week2/Day3/DailyChallenge/script.js
@@ -15,3 +15,44 @@ for (i = 0; i < 7; i++) {
   console.log(str);
   str = "";
 }
+
+//Gold
+
+let numbers = [5, 0, 9, 1, 7, 4, 2, 6, 3, 8];
+
+function bubbleSort(numbers) {
+  console.log(numbers);
+  for (i = 0; i < numbers.length; i++) {
+    for (j = 0; j < numbers.length - 1; j++) {
+      if (numbers[j] > numbers[j + 1]) {
+        let tmp = numbers[j];
+        numbers[j] = numbers[j + 1];
+        numbers[j + 1] = tmp;
+        console.log(numbers);
+      }
+      console.log(numbers);
+    }
+  }
+}
+
+function bubbleSort2(numbers) {
+  console.log(numbers);
+  for (i = 0; i < numbers.length; i++) {
+    for (j = numbers.length; j >= 0; j--) {
+      if (numbers[j + 1] > numbers[j]) {
+        let tmp = numbers[j];
+        numbers[j] = numbers[j + 1];
+        numbers[j + 1] = tmp;
+        console.log(numbers);
+      }
+      console.log(numbers);
+    }
+  }
+}
+
+bubbleSort(numbers);
+bubbleSort2(numbers);
+
+let numbers2 = [5, 0, 9, 1, 7, 4, 2, 6, 3, 8];
+numbers2.toString();
+numbers2.join("+");
