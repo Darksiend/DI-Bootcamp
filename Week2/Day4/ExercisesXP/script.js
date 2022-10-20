9ef7eae7c8e90b06ee7636adbb48ee8bbc627f9b
Darksiend <logosmk2@gmail.com>
2022-10-20 12:00:40 +0300

    Ex1Day1EXPDone

--- Week2/Day4/ExercisesXP/script.js
+++ Week2/Day4/ExercisesXP/script.js
@@ -4,3 +4,13 @@ function infoAboutMe() {
   console.log("Hi Im Anton!!!");
 }
 infoAboutMe();
+
+function infoAboutPerson(personName, personAge, personFavoriteColor) {
+  console.log(
+    `You name is ${personName}, you are ${personAge} years old, your favorite color is ${personFavoriteColor}`
+  );
+}
+
+infoAboutPerson("Anton", 23, "blue");
+infoAboutPerson("David", 45, "blue");
+infoAboutPerson("Josh", 12, "yellow");
