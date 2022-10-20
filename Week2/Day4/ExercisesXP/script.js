9c4f363a141f129244c0b1c00d67f95d08e06518
Darksiend <logosmk2@gmail.com>
2022-10-20 12:59:33 +0300

    Ex4 Done

--- Week2/Day4/ExercisesXP/script.js
+++ Week2/Day4/ExercisesXP/script.js
@@ -42,4 +42,54 @@ function calculateTip() {
   }
 }
 
-calculateTip();
+//calculateTip();
+
+//Ex3
+
+function isDivisible(divisor) {
+  let outcome = "";
+  let sum = 0;
+  for (i = 0; i < 500; i++) {
+    if (i % divisor === 0) {
+      outcome += i + " ";
+      sum = +i;
+    }
+  }
+  console.log(`Outcome: ${outcome} \n and Sum: ${sum}`);
+}
+
+isDivisible(3);
+isDivisible(45);
+
+//Ex4
+
+const stock = {
+  banana: 6,
+  apple: 0,
+  pear: 12,
+  orange: 32,
+  blueberry: 1,
+};
+
+const prices = {
+  banana: 4,
+  apple: 2,
+  pear: 1,
+  orange: 1.5,
+  blueberry: 10,
+};
+
+let shoppingList = ["banana", "orange", "apple"];
+
+function myBill() {
+  let price = 0;
+  for (i = 0; i < shoppingList.length; i++) {
+    if (shoppingList[i] in stock) {
+      price += prices[shoppingList[i]];
+    }
+  }
+  return price;
+}
+
+let price = myBill();
+console.log(price);
