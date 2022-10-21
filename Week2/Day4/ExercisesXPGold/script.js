cd995e2997ecfda456f98cfebdd7d8f893e4f2ae
Darksiend <logosmk2@gmail.com>
2022-10-21 11:29:28 +0300

    Ex3 Done

--- Week2/Day4/ExercisesXPGold/script.js
+++ Week2/Day4/ExercisesXPGold/script.js
@@ -10,3 +10,28 @@ function isBlank(string) {
 console.log(isBlank("vnjdfklnvdf"));
 
 //Ex2
+
+//console.log(abbrevName("Robin Singh")); --> "Robin S."
+
+function abbrevName(name) {
+  let arr = name.split(" ");
+  return arr[0] + " " + arr[1][0].toUpperCase() + ".";
+}
+
+console.log(abbrevName("Anton Chesnokov"));
+
+//Ex3
+
+function swap(sent) {
+  let arr = sent.split("");
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[i].toUpperCase()) {
+      arr[i] = arr[i].toLowerCase();
+    } else if (arr[i] === arr[i].toLowerCase()) {
+      arr[i] = arr[i].toUpperCase();
+    }
+  }
+  console.log(arr.join(""));
+}
+
+swap("The Quick Brown Fox");
