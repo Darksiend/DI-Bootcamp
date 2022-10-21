987eb8e468d0bd341a8f731bedb0baaf5188aae5
Darksiend <logosmk2@gmail.com>
2022-10-21 12:56:27 +0300

    Ex2 Done

--- Week2/Day4/ExercisesXPNinja/script.js
+++ Week2/Day4/ExercisesXPNinja/script.js
@@ -6,3 +6,19 @@ for (let i = 0; i < number; i++) {
   }
 }
 console.log(number);
+
+//Ex2
+
+function capitalize(string) {
+  arr = string.split("");
+  for (i = 0; i < arr.length; i++) {
+    if (i % 2 === 0 && i != 0) {
+      arr[i] = arr[i].toLowerCase();
+    } else {
+      arr[i] = arr[i].toUpperCase();
+    }
+  }
+  return arr.join("");
+}
+
+console.log(capitalize("fdbjkxv"));
