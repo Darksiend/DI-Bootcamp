078aad49731fd1bc61267148b810198209c3111e
Darksiend <logosmk2@gmail.com>
2022-10-22 15:35:30 +0300

    Mini Proj Done!

--- Week2/Day5/MiniProject/script.js
+++ Week2/Day5/MiniProject/script.js
@@ -1,6 +1,40 @@
 //Mini Project
 
 function playTheGame() {
-  if (confirm("Press a button!\nEither OK or Cancel.")) {
+  if (confirm("You wont play?")) {
+    let number = prompt("Enter a number:")
+    if (isNaN(number)) {
+      while (isNaN(number)) {
+        number = prompt("Its need to be a number!")
+      }
+    } else if (number < 0 && number > 10) {
+      alert("Sorry its not a good number, Goodbye")
+    } else {
+      let computerNumber = Math.floor(Math.random() * 11)
+      console.log(`Computer number is: ${computerNumber}`)
+      compareNumbers(number, computerNumber)
+    }
+  } else {
+    alert("No problem, Goodbye")
+  }
+}
+
+function compareNumbers(userNumber, computerNumber) {
+  while (userNumber !== computerNumber) {
+    if (userNumber > computerNumber) {
+      userNumber = prompt(
+        "Your number is bigger then the computers, guess again"
+      )
+      console.log(`New User Number: ${userNumber}`)
+    } else if (userNumber < computerNumber) {
+      userNumber = prompt(
+        "You number is smaller then the computer, guess again"
+      )
+      console.log(`New User Number: ${userNumber}`)
+    }
+    if (userNumber == computerNumber) {
+      alert("Winner!")
+      break
+    }
   }
 }
