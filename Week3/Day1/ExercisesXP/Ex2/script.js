bc7ece37c0e4bf7d545b6a335358385153b12463
Darksiend <logosmk2@gmail.com>
2022-10-23 13:29:39 +0300

    Ex3Done

--- Week3/Day1/ExercisesXP/Ex2/script.js
+++ Week3/Day1/ExercisesXP/Ex2/script.js
@@ -1,1 +1,23 @@
 //Ex2
+
+document.querySelector("div").style.backgroundColor = "lightblue"
+document.querySelector("div").style.padding = "15px"
+for (i = 0; i < document.querySelectorAll("li").length; i++) {
+  if (document.querySelectorAll("li")[i].innerHTML == "John") {
+    document.querySelectorAll("li")[i].style.display = "none"
+  }
+  if (document.querySelectorAll("li")[i].innerHTML == "Pete") {
+    document.querySelectorAll("li")[i].style.border = "1px solid black"
+  }
+}
+document.body.style.fontSize = "45px"
+
+//Bonus: If the background color of the div is “light blue”, alert “Hello x and y” (x and y are the users in the div).
+
+if (document.querySelector("div").style.backgroundColor === "lightblue") {
+  alert(
+    `Hello ${document.querySelectorAll("li")[0].innerHTML} and ${
+      document.querySelectorAll("li")[1].innerHTML
+    }`
+  )
+}
---
+++ Week3/Day1/ExercisesXP/Ex3/script.js
@@ -0,0 +1,10 @@
+//Ex 1
+
+document.getElementById("navBar").setAttribute("id", "socialNetworkNavigation")
+
+const newLi = document.createElement("li")
+const newContent = document.createTextNode("Logout")
+newLi.append(newContent)
+document.querySelector("ul").appendChild(newLi)
+console.log(document.querySelector("ul").firstElementChild.textContent)
+console.log(document.querySelector("ul").lastElementChild.textContent)
