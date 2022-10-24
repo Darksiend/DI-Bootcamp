08038e898e59ca988d65153123762eb972401072
darksiend <logosmk2@icloud.com>
2022-10-24 18:58:36 +0300

    Ex2 Done

--- Week3/Day2/ExercisesXP/Ex2/script.js
+++ Week3/Day2/ExercisesXP/Ex2/script.js
@@ -4,24 +4,29 @@ const firsNameInput = document.getElementById("fname");
 const lastNameInput = document.getElementById("lname");
 const submitBtn = document.getElementById("submit");
 let allInputs = document.querySelectorAll("input");
-console.log(firsNameInput);
-console.log(lastNameInput);
-console.log(submitBtn);
-console.log(form);
-
-console.log(document.getElementsByName("fname"));
-console.log(document.getElementsByName("lname"));
 
 form.addEventListener("submit", function () {
   event.preventDefault();
-  console.log("form: ", form);
-  console.log("first name: ", firsNameInput.value);
-  console.log("last name: ", lastNameInput.value);
+  let allInputs = document.querySelectorAll("input");
+  let firstName = firsNameInput.value;
+  let lastName = lastNameInput.value;
+  let emptyInputs = false;
   for (i = 0; i < allInputs.length - 1; i++) {
     if (!allInputs[i].value) {
       console.log("Field is blank", allInputs[i]);
+      emptyInputs = true;
     }
   }
+  if (emptyInputs == true) {
+    console.log("inputs not filled");
+  } else {
+    let firstNameLiTag = document.createElement("li");
+    let lastNameLiTag = document.createElement("li");
+    ulTag = document.querySelector("ul");
+    firstNameLiTag.innerHTML = firstName;
+    lastNameLiTag.innerHTML = lastName;
+    document.querySelector("ul").append(firstNameLiTag);
+    document.querySelector("ul").append(lastNameLiTag);
+    ulTag.append(firstNameLiTag);
+  }
 });
-
-console.log(document.querySelectorAll("input"));
