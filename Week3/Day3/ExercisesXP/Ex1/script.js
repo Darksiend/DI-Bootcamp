755779344e08a38f7b34cf4098eee127483996b6
Darksiend <logosmk2@gmail.com>
2022-10-25 10:54:45 +0300

    Ex1 Done

--- Week3/Day3/ExercisesXP/Ex1/script.js
+++ Week3/Day3/ExercisesXP/Ex1/script.js
@@ -3,5 +3,31 @@
 function helloWord() {
   alert("Hello Word!");
 }
+function addPara() {
+  let newPar = document.createElement("p");
+  newPar.innerHTML = "Hello World";
+  document.getElementById("container").append(newPar);
+  console.log("<p> Added");
+}
+
+function runXTimes(x, interval, myFunction) {
+  step = 0;
+
+  function stepPlus() {
+    if (step < x) {
+      return (step += 1), myFunction();
+    } else {
+      return clearInterval(id);
+    }
+  }
+  return (id = setInterval(stepPlus, interval));
+}
+
+function clickOnButton() {
+  console.log("clicked");
+  clearInterval(id);
+}
+
+runXTimes(5, 2000, addPara);
 
 setTimeout(helloWord, 2000);
