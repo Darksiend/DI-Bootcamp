4c33d890fc58e8f65ea0b487d91995af96558338
Darksiend <logosmk2@gmail.com>
2022-10-25 09:16:13 +0300

    Work

--- Week3/Day1/DailyChallenge /script.js
+++ Week3/Day1/DailyChallenge /script.js
@@ -1,43 +1,31 @@
 let planetsList = [
-  { nameofPlanet: "Mercury", numberofMoons: 0 },
-  { nameofPlanet: "Venus", numberofMoons: 0 },
-  { nameofPlanet: "Earth", numberofMoons: 1 },
-  { nameofPlanet: "Mars", numberofMoons: 2 },
-  { nameofPlanet: "Jupiter", numberofMoons: 2 },
-  { nameofPlanet: "Saturn", numberofMoons: 3 },
-  { nameofPlanet: "Uranus", numberofMoons: 2 },
-  { nameofPlanet: "Neptune", numberofMoons: 2 },
-  { nameofPlanet: "Pluto", numberofMoons: 3 },
-]
-let colorsArr = [
-  "red",
-  "blue",
-  "white",
-  "orange",
-  "blueViolet",
-  "brown",
-  "gold",
-  "wheat",
-  "yellow",
-]
-let parent = document.querySelector("section")
-console.log(planetsList.length)
+  { nameofPlanet: "Mercury", numberofMoons: 0, color: "red" },
+  { nameofPlanet: "Venus", numberofMoons: 0, color: "blue" },
+  { nameofPlanet: "Earth", numberofMoons: 1, color: "white" },
+  { nameofPlanet: "Mars", numberofMoons: 2, color: "orange" },
+  { nameofPlanet: "Jupiter", numberofMoons: 2, color: "blueViolet" },
+  { nameofPlanet: "Pluto", numberofMoons: 3, color: "brown" },
+  { nameofPlanet: "Saturn", numberofMoons: 3, color: "gold" },
+  { nameofPlanet: "Uranus", numberofMoons: 2, color: "wheat" },
+  { nameofPlanet: "Neptune", numberofMoons: 2, color: "yellow" },
+];
+let parent = document.querySelector("section");
+console.log(planetsList.length);
 for (i = 0; i < planetsList.length; i++) {
-  let planet = document.createElement("div")
-
-  planet.style.backgroundColor = colorsArr[i]
-  planet.classList.add("planet")
-  planet.style.display = "flex"
-  planet.style.alignItems = "center"
-  planet.style.justifyContent = "center"
-  parent.append(planet)
+  let planet = document.createElement("div");
+  planet.classList.add("planet");
+  planet.style.display = "flex";
+  planet.style.alignItems = "center";
+  planet.style.justifyContent = "center";
+  planet.style.backgroundColor = planetsList[i].color;
+  parent.append(planet);
   if (planetsList[i].numberofMoons > 0) {
     for (j = 0; j < planetsList[i].numberofMoons; j++) {
-      let moon = document.createElement("div")
-      planet.append(moon)
-      moon.classList.add("moon")
-      moon.style = "position:relative"
+      let moon = document.createElement("div");
+      planet.append(moon);
+      moon.classList.add("moon");
+      moon.style = "position:relative";
     }
   }
 }
-console.log(parent)
+console.log(parent);
