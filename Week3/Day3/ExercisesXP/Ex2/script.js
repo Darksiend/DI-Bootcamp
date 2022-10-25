6c352dc4e2717e1c8135b7e6d5e92cdce4cf4736
Darksiend <logosmk2@gmail.com>
2022-10-25 12:37:56 +0300

    Ex2 Done

--- Week3/Day3/ExercisesXP/Ex2/script.js
+++ Week3/Day3/ExercisesXP/Ex2/script.js
@@ -1,1 +1,18 @@
 //Ex2
+let marginLeft = 0;
+let counter = 0;
+//let object =
+//let container = document.getElementById("container");
+function animate() {
+  let pixels = 1;
+  counter += 1;
+  if (counter <= 350) {
+    marginLeft += pixels;
+    document.getElementById("animate").style.marginLeft = `${marginLeft}px`;
+    console.log(`${marginLeft}px`);
+  } else {
+    clearInterval(id);
+  }
+}
+
+id = setInterval(animate, 10);
