c79969c945528a2ba2ba186d670e7bd7512946b7
Darksiend <logosmk2@gmail.com>
2022-10-26 11:16:03 +0300

    Start of changing Task Object

--- Week3/Day4/ToDo/script.js
+++ Week3/Day4/ToDo/script.js
@@ -14,18 +14,49 @@ function addTask() {
   if (inputField.value != "") {
     let taskElement = document.createElement("div");
 
+    let deleteTaskIcon = document.createElement("img");
+
+    deleteTaskIcon.setAttribute("src", "assets/delete.svg");
+    deleteTaskIcon.setAttribute("id", "delete-button");
     let inputCheckbox = document.createElement("input");
 
     inputCheckbox.setAttribute("type", "checkbox");
 
+    inputCheckbox.setAttribute("class", "do-checkbox");
+
     taskElement.textContent = inputField.value;
 
-    taskElement.append(inputCheckbox);
+    taskElement.setAttribute("class", "task-item");
 
     tasksArr.push(taskElement.textContent);
 
+    taskElement.prepend(inputCheckbox);
+
+    taskElement.prepend(deleteTaskIcon);
+
+    taskElement.style.display = "flex";
+
+    taskElement.style.alignItems = "center";
+
     tasksList.append(taskElement);
+
+    inputField.value = "";
 
     console.log(tasksArr);
   } else return;
 }
+
+document.addEventListener("change", doTask);
+
+function doTask(event) {
+  console.log(event.target.checked);
+  if (event.target.checked == false) {
+    event.path[1].style.textDecoration = "none";
+  } else {
+    console.log("UnChecked");
+    console.log(event.path[1]);
+    event.path[1].style.textDecoration = "line-through";
+  }
+}
+
+function deleteTask() {}
