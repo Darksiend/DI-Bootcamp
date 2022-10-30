acb600e1a25151ca7f62ebdd250a04f260f7abd0
Darksiend <logosmk2@gmail.com>
2022-10-30 12:56:22 +0200

    Daily Challenge Done

---
+++ Week4/Day1/Daily Challenge/script.js
@@ -0,0 +1,38 @@
+const gameInfo = [
+  {
+    username: "john",
+    team: "red",
+    score: 5,
+    items: ["ball", "book", "pen"],
+  },
+  {
+    username: "becky",
+    team: "blue",
+    score: 10,
+    items: ["tape", "backpack", "pen"],
+  },
+  {
+    username: "susy",
+    team: "red",
+    score: 55,
+    items: ["ball", "eraser", "pen"],
+  },
+  {
+    username: "tyson",
+    team: "green",
+    score: 1,
+    items: ["book", "pen"],
+  },
+];
+let usernames = [];
+let moreThan = [];
+gameInfo.forEach(function (obj) {
+  usernames.push(obj.username + "!");
+});
+
+gameInfo.forEach(function (obj) {
+  if (obj.score > 5) moreThan.push(obj.username + "!");
+});
+
+console.log(usernames);
+console.log(moreThan);
