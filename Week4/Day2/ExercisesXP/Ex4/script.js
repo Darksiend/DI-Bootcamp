42da18ffd5690882344cc6403158e8666a876d89
Darksiend <logosmk2@gmail.com>
2022-10-31 10:50:19 +0200

    Ex4 Done

--- Week4/Day2/ExercisesXP/Ex4/script.js
+++ Week4/Day2/ExercisesXP/Ex4/script.js
@@ -1,1 +1,10 @@
 //Ex 4
+
+((name) => {
+  let div = document.createElement("div");
+  let userNameDiv = document.createElement("div");
+  let userNamePicture = document.createElement("img");
+  userNameDiv.textContent = name;
+  div.append(userNameDiv);
+  div.append(userNamePicture);
+})("Anton");
