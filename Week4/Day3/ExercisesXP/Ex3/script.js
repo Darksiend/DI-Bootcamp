eb3645ce6f6d561825276268718fd2ef9b373e1a
Darksiend <logosmk2@gmail.com>
2022-11-02 09:31:35 +0200

    Ex3

--- Week4/Day3/ExercisesXP/Ex3/script.js
+++ Week4/Day3/ExercisesXP/Ex3/script.js
@@ -1,1 +1,8 @@
 //Ex 3
+const epic = ["a", "long", "time", "ago", "in a", "galaxy", "far far", "away"];
+
+let res = epic.reduce((prev, next) => {
+  return prev + " " + next;
+});
+
+console.log(res);
