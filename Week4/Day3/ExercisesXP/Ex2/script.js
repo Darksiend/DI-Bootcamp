11028f892992c5df8cc915e81bd7fc233060a3a4
Darksiend <logosmk2@gmail.com>
2022-11-02 09:16:30 +0200

    Ex2Done

---
+++ Week4/Day3/ExercisesXP/Ex2/script.js
@@ -0,0 +1,38 @@
+//Ex2
+const users = [
+  { firstName: "Bradley", lastName: "Bouley", role: "Full Stack Resident" },
+  { firstName: "Chloe", lastName: "Alnaji", role: "Full Stack Resident" },
+  { firstName: "Jonathan", lastName: "Baughn", role: "Enterprise Instructor" },
+  { firstName: "Michael", lastName: "Herman", role: "Lead Instructor" },
+  { firstName: "Robert", lastName: "Hajek", role: "Full Stack Resident" },
+  { firstName: "Wes", lastName: "Reid", role: "Instructor" },
+  { firstName: "Zach", lastName: "Klabunde", role: "Instructor" },
+];
+
+const welcomeStudents = users
+  .filter((el) => el.role === "Full Stack Resident")
+  .map((el) => {
+    return `Hello ${el.firstName}`;
+  });
+
+console.log(welcomeStudents);
+
+//Bonus
+
+const usersBonus = [
+  { firstName: "Bradley", lastName: "Bouley", role: "Full Stack Resident" },
+  { firstName: "Chloe", lastName: "Alnaji", role: "Full Stack Resident" },
+  { firstName: "Jonathan", lastName: "Baughn", role: "Enterprise Instructor" },
+  { firstName: "Michael", lastName: "Herman", role: "Lead Instructor" },
+  { firstName: "Robert", lastName: "Hajek", role: "Full Stack Resident" },
+  { firstName: "Wes", lastName: "Reid", role: "Instructor" },
+  { firstName: "Zach", lastName: "Klabunde", role: "Instructor" },
+];
+
+studentsBonus = usersBonus
+  .filter((el) => el.role === "Full Stack Resident")
+  .map((el) => {
+    return el.lastName;
+  });
+
+console.log(studentsBonus);
