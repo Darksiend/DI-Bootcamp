f76970fb35a3e17328742ee5e5ae5080a4f55161
Darksiend <logosmk2@gmail.com>
2022-11-02 09:56:11 +0200

    Ex 4 Done

---
+++ Week4/Day3/ExercisesXP/Ex4/script.js
@@ -0,0 +1,19 @@
+//Ex 4
+
+const students = [
+  { name: "Ray", course: "Computer Science", isPassed: true },
+  { name: "Liam", course: "Computer Science", isPassed: false },
+  { name: "Jenner", course: "Information Technology", isPassed: true },
+  { name: "Marco", course: "Robotics", isPassed: true },
+  { name: "Kimberly", course: "Artificial Intelligence", isPassed: false },
+  { name: "Jamie", course: "Big Data", isPassed: false },
+];
+let resArr = [];
+const passed = students
+  .filter((student) => student.isPassed === true)
+  .forEach((el) =>
+    console.log(
+      `Good job ${el.name}, you passed the course in Information Technology`
+    )
+  );
+console.log(resArr);
