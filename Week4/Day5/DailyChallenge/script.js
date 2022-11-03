0eca4ecccf04564ea135353280a12222a888d0e9
Anton Chesnokov <logosmk2@gmail.com>
2022-11-03 11:11:52 +0200

    DC Done

--- Week4/Day5/DailyChallenge/script.js
+++ Week4/Day5/DailyChallenge/script.js
@@ -1,1 +1,10 @@
 //Daily Challenge: Anagram
+
+function ifAnagram(stringOne, stringTwo) {
+  return (
+    stringOne.toLowerCase().replace(/ /g, "").split("").sort().toString() ===
+    stringTwo.toLowerCase().replace(/ /g, "").split("").sort().toString()
+  );
+}
+
+console.log(ifAnagram("Astronomer", "Moon starer"));
