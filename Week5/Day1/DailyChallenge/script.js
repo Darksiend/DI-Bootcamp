601d43290eb624485c5225a46e5c8ee1ba7729ab
Darksiend <logosmk2@gmail.com>
2022-11-05 16:51:54 +0200

    Work

--- Week5/Day1/DailyChallenge/script.js
+++ Week5/Day1/DailyChallenge/script.js
@@ -1,3 +1,12 @@
 //DailyChallenge
 
-document.getElementById("form").addEventListener("submit", () => {});
+document.getElementById("form").addEventListener("submit", () => {
+  this.event.preventDefault();
+  let obj = {};
+  obj[this.event.target[0].id] = this.event.target[0].value;
+  obj[this.event.target[1].id] = this.event.target[1].value;
+
+  console.log(this.event.target[0].value);
+  console.log(this.event.target[1].id);
+  console.log(obj);
+});
