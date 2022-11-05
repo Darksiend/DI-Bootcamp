b68bcae9c8fb7c7be472fef49cb2577cb53de29c
Darksiend <logosmk2@gmail.com>
2022-11-05 17:21:13 +0200

    Work

--- Week5/Day2/Ex1/script.js
+++ Week5/Day2/Ex1/script.js
@@ -4,10 +4,12 @@ let xhr = new XMLHttpRequest();
 
 xhr.open(
   "GET",
-  " https://api.giphy.com/v1/gifs/search?q=hilarious&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My"
+  "https://api.giphy.com/v1/gifs/search?q=sun&rating=g&limit=10&offset:2&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My"
 );
 xhr.send();
 
 xhr.onload = () => {
-  console.log(JSON.parse(xhr.response));
+  console.log(JSON.parse(xhr.response).data);
 };
+
+//Ex2
