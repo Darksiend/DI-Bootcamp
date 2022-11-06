9e867d64d4ae44fed9daa3bae7f1accbd2383f2e
Darksiend <logosmk2@gmail.com>
2022-11-06 11:41:06 +0200

    Ex1 Done

--- Week5/Day3/Exercise XP /Ex1/script.js
+++ Week5/Day3/Exercise XP /Ex1/script.js
@@ -1,1 +1,19 @@
 //Ex1
+function compareToTen(num) {
+  return new Promise((resolve, reject) => {
+    if (num < 10) {
+      resolve("Resolve");
+    } else if (num > 10) {
+      reject("Reject");
+    }
+  });
+}
+
+compareToTen(15)
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error));
+
+//In the example, the promise should resolve
+compareToTen(8)
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error));
