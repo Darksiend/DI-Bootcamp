e92b1e3c069811e03c2af4922b57b8cce070d846
Darksiend <logosmk2@gmail.com>
2022-11-06 12:15:51 +0200

    Work

--- Week5/Day3/ExerciseXPGold/script.js
+++ Week5/Day3/ExerciseXPGold/script.js
@@ -1,1 +1,10 @@
 //XPGOLD
+const urls = [
+  "https://www.swapi.tech/api/people/1",
+  "https://www.swapi.tech/api/people/2",
+  "https://www.swapi.tech/api/people/3",
+  "https://www.swapi.tech/api/people/4",
+];
+
+let promise = Promise.all(urls);
+console.log(promise);
