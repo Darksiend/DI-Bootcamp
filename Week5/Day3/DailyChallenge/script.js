a640069f12c40484864654d9fb4a43d2833457c3
Darksiend <logosmk2@gmail.com>
2022-11-07 21:13:36 +0200

    DC Done

--- Week5/Day3/DailyChallenge/script.js
+++ Week5/Day3/DailyChallenge/script.js
@@ -1,4 +1,4 @@
-//DC
+//DC 1
 arr = ["Apple", "Orange", "Peach", "Tea", "Cofee"];
 
 function makeAllCaps(arr) {
@@ -28,3 +28,88 @@ function sortWords(arr) {
 }
 
 makeAllCaps(arr).then(sortWords(arr).then(console.log(arr)));
+
+//DC 2
+let morse = `{
+  "0": "-----",
+  "1": ".----",
+  "2": "..---",
+  "3": "...--",
+  "4": "....-",
+  "5": ".....",
+  "6": "-....",
+  "7": "--...",
+  "8": "---..",
+  "9": "----.",
+  "a": ".-",
+  "b": "-...",
+  "c": "-.-.",
+  "d": "-..",
+  "e": ".",
+  "f": "..-.",
+  "g": "--.",
+  "h": "....",
+  "i": "..",
+  "j": ".---",
+  "k": "-.-",
+  "l": ".-..",
+  "m": "--",
+  "n": "-.",
+  "o": "---",
+  "p": ".--.",
+  "q": "--.-",
+  "r": ".-.",
+  "s": "...",
+  "t": "-",
+  "u": "..-",
+  "v": "...-",
+  "w": ".--",
+  "x": "-..-",
+  "y": "-.--",
+  "z": "--..",
+  ".": ".-.-.-",
+  ",": "--..--",
+  "?": "..--..",
+  "!": "-.-.--",
+  "-": "-....-",
+  "/": "-..-.",
+  "@": ".--.-.",
+  "(": "-.--.",
+  ")": "-.--.-"
+}`;
+
+function toJs(json) {
+  return new Promise((resolve, reject) => {
+    if (json !== "") {
+      let morseJS = JSON.parse(morse);
+      resolve(morseJS);
+    } else {
+      reject("Empty");
+    }
+  });
+}
+
+function toMorse(morseJS) {
+  return new Promise((resolve, reject) => {
+    let userWord = prompt("Put the word");
+    let res = [];
+    let arrUserWord = userWord.split("");
+    for (let i = 0; i < arrUserWord.length; i++) {
+      res.push(morseJS[arrUserWord[i]]);
+      if (morseJS[arrUserWord[i]] === undefined) {
+        reject("Undefiend Character");
+      }
+    }
+    resolve(res);
+  });
+}
+
+toJs(morse).then((morseJS) => {
+  toMorse(morseJS).then((res) => {
+    joinWords(res);
+  });
+});
+
+function joinWords(morseTranslation) {
+  console.log(morseTranslation.join(`\n`));
+}
