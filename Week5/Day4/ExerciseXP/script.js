bc4cef0b0c66b255cef8bf34fed2a936e26d32e9
Darksiend <logosmk2@gmail.com>
2022-11-08 08:17:43 +0200

    Work

--- Week5/Day4/ExerciseXP/script.js
+++ Week5/Day4/ExerciseXP/script.js
@@ -5,7 +5,7 @@ fetch("https://www.swapi.tech/api/starships/9/")
   .then((result) => console.log(result));
 
 async function fetchFromUrl() {
-  await fetch("https://www.swapi.tech/api/starships/9/")
+  return await fetch("https://www.swapi.tech/api/starships/9/")
     .then((response) => response.json())
     .then((result) => console.log(result));
 }
