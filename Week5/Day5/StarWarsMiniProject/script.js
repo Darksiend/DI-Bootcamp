f7876ec4b8f84f3e6f8572021814522374befb29
Darksiend <logosmk2@gmail.com>
2022-11-08 10:38:23 +0200

    Work

--- Week5/Day5/StarWarsMiniProject/script.js
+++ Week5/Day5/StarWarsMiniProject/script.js
@@ -1,1 +1,23 @@
 //StarwarsMiniProject
+function loadCharacter() {
+  console.log("click!");
+  xhr = new XMLHttpRequest();
+  xhr.open("GET", `https://www.swapi.tech/api/people/1`);
+  xhr.send();
+  console.log(xhr);
+  xhr.onload = () => {
+    console.log(JSON.parse(xhr.response));
+    let data = JSON.parse(xhr.response);
+  };
+
+  xhr.onprogress = function (event) {
+    if (event.lengthComputable) {
+      console.log(`Received ${event.loaded} of ${event.total} bytes`);
+    } else {
+      console.log(`Received ${event.loaded} bytes`); // no Content-Length
+    }
+  };
+  xhr.onerror = function () {
+    console.log("Error something wrong###");
+  };
+}
