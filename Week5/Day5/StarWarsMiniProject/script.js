2930549c63dbda9b98f8a344c6a06509af9ca8ae
Darksiend <logosmk2@gmail.com>
2022-11-08 11:43:36 +0200

    DC Done

--- Week5/Day5/StarWarsMiniProject/script.js
+++ Week5/Day5/StarWarsMiniProject/script.js
@@ -3,18 +3,28 @@ function loadCharacter(qualifiedName, value) {
   document.getElementById("character-info-container").style.display = "none";
   document.getElementById("donut").style.display = "inline-block";
   console.log("click!");
+  let randChar = Math.floor(Math.random() * 86);
   xhr = new XMLHttpRequest();
-  xhr.open("GET", `https://www.swapi.tech/api/people/1`);
+  xhr.open("GET", `https://www.swapi.tech/api/people/${randChar}`);
   xhr.send();
   console.log(xhr);
 
   xhr.onload = () => {
-    console.log(JSON.parse(xhr.response).result.properties);
-    let character = JSON.parse(xhr.response).result.properties;
-    console.log("loaded");
-    displayCharacter(character);
-    document.getElementById("donut").style.display = "none";
-    document.getElementById("character-info-container").style.display = "flex";
+    if (xhr.status != 200) {
+      document.getElementById("character-info-container").textContent =
+        "Dont Have This Character";
+      document.getElementById("character-info-container").style.display =
+        "flex";
+      document.getElementById("donut").style.display = "none";
+    } else {
+      console.log(JSON.parse(xhr.response).result.properties);
+      let character = JSON.parse(xhr.response).result.properties;
+      console.log("loaded");
+      displayCharacter(character);
+      document.getElementById("donut").style.display = "none";
+      document.getElementById("character-info-container").style.display =
+        "flex";
+    }
   };
 
   xhr.onerror = function () {
