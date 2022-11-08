64f7a03f0d8af56f86c49dc3c7597339dc24943d
Darksiend <logosmk2@gmail.com>
2022-11-08 08:30:33 +0200

    Work

---
+++ Week5/Day4/DailyChallengePromises/DailyChalenge1/script.js
@@ -0,0 +1,16 @@
+//DC
+
+const promise1 = Promise.resolve(3);
+const promise2 = 42;
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 3000, "foo");
+});
+//  expected output: Array [3, 42, "foo"]
+
+Promise.all([promise1, promise2, promise3])
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
