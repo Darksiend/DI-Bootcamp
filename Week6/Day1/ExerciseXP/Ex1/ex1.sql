7927f842dcbed102df11a308d27c0a4380dbf853
Darksiend <logosmk2@gmail.com>
2022-11-10 10:09:02 +0200

    Added Data To Items

--- Week6/Day1/ExerciseXP/Ex1/ex1.sql
+++ Week6/Day1/ExerciseXP/Ex1/ex1.sql
@@ -3,3 +3,10 @@ CREATE TABLE items(
     item_name TEXT NOT NULL,
     item_price INTEGER
 )
+
+INSERT INTO items
+VALUES
+    (1, 'Small Desk',100)
+    (2,'Large desk',300)
+    (3,'Fan',80)
+    
