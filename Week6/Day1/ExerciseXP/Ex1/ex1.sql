e7a7e5756e7651eb8edd693f4302be23e3775fde
Darksiend <logosmk2@gmail.com>
2022-11-10 10:18:43 +0200

    Work

--- Week6/Day1/ExerciseXP/Ex1/ex1.sql
+++ Week6/Day1/ExerciseXP/Ex1/ex1.sql
@@ -6,7 +6,20 @@ CREATE TABLE items(
 
 INSERT INTO items
 VALUES
-    (1, 'Small Desk',100)
-    (2,'Large desk',300)
-    (3,'Fan',80)
+    (1, 'Small Desk',100),
+    (2,'Large desk',300),
+    (3,'Fan',80);
     
+CREATE TABLE customers(
+    costumer_id INTEGER PRIMARY KEY,
+    costumer_first_name TEXT NOT NULL,
+    costumer_last_name TEXT NOT NULL
+)
+
+INSERT INTO customers
+VALUES
+    (1,'Greg','Jones'),
+    (2,'Sandra','Jones'),
+    (3,'Scott','Scott'),
+    (4,'Trevor','Green'),
+    (5,'Melanie','Johnson')
