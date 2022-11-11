713d412ebb0f6b0d2cf7eb25a2201238167a58b5
Darksiend <logosmk2@gmail.com>
2022-11-11 17:25:39 +0200

    Add coments

--- Week6/Day1/ExerciseXP+/ex1.sql
+++ Week6/Day1/ExerciseXP+/ex1.sql
@@ -18,12 +18,12 @@ VALUES
 SELECT * FROM students
 
 SELECT last_name,first_name  FROM students
-
+-- Fetch the student which id is equal to 2.
 SELECT last_name,first_name  FROM students WHERE id = 2
-
+-- Fetch the student whose last_name is Benichou AND first_name is Marc.
 SELECT *  FROM students WHERE last_name = 'Benichou' AND first_name = 'Marc'
-
+-- Fetch the students whose last_names are Benichou OR first_names are Marc.
 SELECT *  FROM students WHERE last_name = 'Benichou' OR first_name = 'Marc'
-
+-- Fetch the students whose first_names contain the letter a.
 SELECT * FROM students WHERE  first_name LIKE '%a%'
 
