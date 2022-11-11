c54e78e46b1c7f6ad141586a249e94bdefb934dc
Darksiend <logosmk2@gmail.com>
2022-11-11 17:38:59 +0200

    Done

--- Week6/Day1/ExerciseXP+/ex1.sql
+++ Week6/Day1/ExerciseXP+/ex1.sql
@@ -21,13 +21,18 @@ SELECT last_name,first_name  FROM students
 -- Fetch the student which id is equal to 2.
 SELECT last_name,first_name  FROM students WHERE id = 2
 -- Fetch the student whose last_name is Benichou AND first_name is Marc.
-SELECT *  FROM students WHERE last_name = 'Benichou' AND first_name = 'Marc'
+SELECT * FROM students WHERE last_name = 'Benichou' AND first_name = 'Marc'
 -- Fetch the students whose last_names are Benichou OR first_names are Marc.
-SELECT *  FROM students WHERE last_name = 'Benichou' OR first_name = 'Marc'
+SELECT * FROM students WHERE last_name = 'Benichou' OR first_name = 'Marc'
 -- Fetch the students whose first_names contain the letter a.
 SELECT * FROM students WHERE  first_name LIKE '%a%'
 -- Fetch the students whose first_names start with the letter a.
 SELECT * FROM students WHERE first_name LIKE 'A%'
 -- Fetch the students whose first_names end with the letter a.
 SELECT * FROM students WHERE first_name LIKE '%a'
-
+-- Fetch the students whose second to last letter of their first_names are a (Example: Leah).
+SELECT * FROM students WHERE first_name LIKE '%a_'
+-- Fetch the students whose id’s are equal to 1 AND 3 .
+SELECT * FROM students WHERE id = 1 or id = 3
+-- Fetch the students whose birth_dates are equal to or come after 1/01/2000. (show all their info).
+SELECT * FROM students WHERE birth_date >= '1/01/2000'
