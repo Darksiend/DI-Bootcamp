52ef0a3f5a1db7bd977156e0491bd53bf22f0ba0
Darksiend <logosmk2@gmail.com>
2022-11-12 22:53:49 +0200

    work

--- Week6/Day2/ExerciseXP/Ex1/ex1.sql
+++ Week6/Day2/ExerciseXP/Ex1/ex1.sql
@@ -1,2 +1,6 @@
 -- All items, ordered by price (lowest to highest).
 SELECT * FROM items ORDER BY item_price  ASC
+-- Items with a price above 80 (80 included), ordered by price (highest to lowest).
+SELECT * FROM items WHERE item_price > 80 ORDER BY item_price  DESC 
+-- The first 3 customers in alphabetical order of the first name (A-Z) – exclude the primary key column from the results.
+
