3cedf15db04cfbc36f410a2ba986a7650211c1c1
Darksiend <logosmk2@gmail.com>
2022-11-18 19:31:55 +0200

    Work

--- Week7/Day1/DailyChallenge/script.js
+++ Week7/Day1/DailyChallenge/script.js
@@ -1,3 +1,10 @@
 const largeNumber = require("./main.js");
 const b = 5;
 console.log(largeNumber + b);
+
+let http = require("http");
+
+const server3d = http.createServer(() => {
+  console.log("Im Listening");
+});
+server.listen(3000);
