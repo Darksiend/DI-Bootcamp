2947476b9a48315b1d05f55c8ebe68583be75fb2
Darksiend <logosmk2@gmail.com>
2022-11-18 19:47:28 +0200

    Work

--- Week7/Day1/DailyChallenge/script.js
+++ Week7/Day1/DailyChallenge/script.js
@@ -4,7 +4,12 @@ console.log(largeNumber + b);
 
 let http = require("http");
 
-const server3d = http.createServer(() => {
-  console.log("Im Listening");
-});
-server.listen(3000);
+const server = http
+  .createServer((req, res) => {
+    console.log("Im Listening");
+    res.setHeader("Content-type", "text/html");
+    res.end(
+      `<p>My Module is ${largeNumber}</p><h1>Hi there at the frontend</h1>`
+    );
+  })
+  .listen(3000);
