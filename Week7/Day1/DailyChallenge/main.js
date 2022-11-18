45148d155274d7bdd9e1f8ba68d5bc0f3c0ec4f9
Darksiend <logosmk2@gmail.com>
2022-11-18 19:58:49 +0200

    Work

--- Week7/Day1/DailyChallenge/main.js
+++ Week7/Day1/DailyChallenge/main.js
@@ -1,3 +1,9 @@
 const largeNumber = 356;
 
 module.exports = largeNumber;
+
+currentDate = () => {
+  return new Date();
+};
+
+module.exports = currentDate;
--- Week7/Day1/DailyChallenge/script.js
+++ Week7/Day1/DailyChallenge/script.js
@@ -1,15 +1,15 @@
 const largeNumber = require("./main.js");
+currentDate = require("./main.js");
 const b = 5;
 console.log(largeNumber + b);
 
 let http = require("http");
-
 const server = http
   .createServer((req, res) => {
     console.log("Im Listening");
     res.setHeader("Content-type", "text/html");
     res.end(
-      `<p>My Module is ${largeNumber}</p><h1>Hi there at the frontend</h1>`
+      `<p>My Module is ${largeNumber}</p><h1>Hi there at the frontend</h1><p>${currentDate()}</p>`
     );
   })
   .listen(3000);
