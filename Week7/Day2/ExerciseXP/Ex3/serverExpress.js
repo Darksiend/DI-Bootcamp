291bcb3033d8feea4338248e84946156da5f3a65
Anton Chesnokov <logosmk2@gmail.com>
2022-11-19 11:31:10 +0200

    Work

---
+++ Week7/Day2/ExerciseXP/Ex3/serverExpress.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, (err) => {
+  err ? console.log(err) : console.log(`listening port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
