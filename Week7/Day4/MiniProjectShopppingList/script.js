d9f282fd6902b5104589f38d6c9b06d69f9aa594
Darksiend <logosmk2@gmail.com>
2022-11-20 20:46:16 +0200

    Work

--- Week7/Day4/MiniProjectShopppingList/script.js
+++ Week7/Day4/MiniProjectShopppingList/script.js
@@ -2,22 +2,45 @@
 const express = require("express");
 const app = express();
 const logger = require("morgan");
-const port = 3001;
+const port = 3002;
+
+let listOfItems = [];
 
 app.use(logger("dev"));
 app.set("view engine", "ejs");
 
+const urlencodedParser = express.urlencoded({ extended: false });
 app
   .route("/")
   .get((req, res) => {
     console.log(req.params);
-    res.render("main", { nameRestaurant: "Papa Pizza" });
+    res.render("main", { listOfItems: listOfItems });
   })
-  .post((req, res) => {
-    console.log();
-    res.render("main", { nameRestaurant: "Papa Pizza" });
+  .post(urlencodedParser, (req, res) => {
+    if (req.body) {
+      addItemToList(req.body, req);
+      console.log(listOfItems);
+      console.log(req.body);
+      res.redirect("/");
+    }
   });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+function addItemToList(itemObj) {
+  console.log(itemObj);
+  if (!ifInList(itemObj)) {
+    listOfItems.push(itemObj);
+  }
+
+  console.log(listOfItems);
+}
+
+function ifInList(obj) {
+  listOfItems.forEach((el) => {
+    if (el === obj) return true;
+  });
+  return false;
+}
---
+++ Week7/Day4/RSSViewerMIniProject/server.js
@@ -0,0 +1,12 @@
+//Rss Viewer
+let Parser = require("rss-parser");
+let parser = new Parser();
+
+(async () => {
+  let feed = await parser.parseURL("https://www.thefactsite.com/feed/");
+  console.log(feed.title);
+
+  feed.items.forEach((item) => {
+    console.log(item.title + ":" + item.link);
+  });
+})();
