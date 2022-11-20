e6ccc5b57f6b3b5e572883ae26b34f9ac61ec8bd
Darksiend <logosmk2@gmail.com>
2022-11-20 21:08:36 +0200

    Work

--- Week7/Day4/RSSViewerMIniProject/server.js
+++ Week7/Day4/RSSViewerMIniProject/server.js
@@ -2,6 +2,25 @@
 let Parser = require("rss-parser");
 let parser = new Parser();
 
+const express = require("express");
+const app = express();
+const logger = require("morgan");
+const port = 3003;
+
+let listOfItems = [];
+
+app.use(logger("dev"));
+app.set("view engine", "ejs");
+
+const urlencodedParser = express.urlencoded({ extended: false });
+app.route("/").get((req, res) => {
+  console.log(req.params);
+  res.render("pages/index");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
 (async () => {
   let feed = await parser.parseURL("https://www.thefactsite.com/feed/");
   console.log(feed.title);
