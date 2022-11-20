7d8cd11893e34c1889c3598bcf67edd711f8216b
Darksiend <logosmk2@gmail.com>
2022-11-20 13:32:39 +0200

    Work

--- Week7/Day4/MiniProjectShopppingList/script.js
+++ Week7/Day4/MiniProjectShopppingList/script.js
@@ -7,9 +7,16 @@ const port = 3001;
 app.use(logger("dev"));
 app.set("view engine", "ejs");
 
-app.route("/").get((req, res) => {
-  res.render("main", { nameRestaurant: "Papa Pizza" });
-});
+app
+  .route("/")
+  .get((req, res) => {
+    console.log(req.params);
+    res.render("main", { nameRestaurant: "Papa Pizza" });
+  })
+  .post((req, res) => {
+    console.log();
+    res.render("main", { nameRestaurant: "Papa Pizza" });
+  });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
