067c8ddff609790b091e9e10395d6dd2143bc4ba
Anton Chesnokov <logosmk2@gmail.com>
2022-11-26 16:11:02 +0200

    Work

---
+++ Week8/Day1/exercises/src/ex1component.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+const Ex1Component = () => {
+    return (
+        <div>
+            <h1>Ex1 component</h1>
+        </div>
+    );
+};
+
+export default Ex1Component;
---
+++ Week8/Day1/exercises/src/ex4component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const h1style = {}
+
+const Ex4Component = () => {
+    return (
+        <div>
+            <h1 style={h1style}>This is a header!</h1>
+        </div>
+    );
+};
+
+export default Ex4Component;
