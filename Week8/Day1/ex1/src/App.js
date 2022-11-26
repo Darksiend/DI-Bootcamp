7c84e54e3a2976d4a6ae81888cc1bbbef68f3ecd
Anton Chesnokov <logosmk2@gmail.com>
2022-11-26 11:44:18 +0200

    Work

--- Week8/Day1/ex1/src/App.js
+++ Week8/Day1/ex1/src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
 function App() {
+  const myelement = <h1>I Love JSX!</h1>;
   return (
     <div className="App">
       <header className="App-header">
