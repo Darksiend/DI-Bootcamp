2617922978bbc32abdd91fe012e123a7bbf9ee84
Darksiend <logosmk2@gmail.com>
2022-11-28 12:17:45 +0200

    Work

--- Week8/Day2/dailychallenge/src/App.js
+++ Week8/Day2/dailychallenge/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      languages: [
+        { name: "Php", votes: 0 },
+        { name: "Python", votes: 0 },
+        { name: "JavaSript", votes: 0 },
+        { name: "Java", votes: 0 },
+      ],
+    };
+  }
+  render(props) {
+    return (
+      <div>
+        {this.state.languages.map((item) => (
+          <div className="voteItem">
+            {item.votes} {item.name} <button>+</button>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
