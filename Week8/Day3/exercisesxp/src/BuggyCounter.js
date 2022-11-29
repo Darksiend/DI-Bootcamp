86d257084713221d91953f8c8f8417e4558f0721
Darksiend <logosmk2@gmail.com>
2022-11-29 10:04:33 +0200

    Work

--- Week8/Day3/exercisesxp/src/BuggyCounter.js
+++ Week8/Day3/exercisesxp/src/BuggyCounter.js
@@ -1,16 +1,20 @@
 import React, { Component } from "react";
 
 class BuggyCounter extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = { counter: 0 };
   }
-  handleClick = () => {};
+
+  handleClick = () => {
+    console.log("Clicked");
+    this.setState({ counter: this.state.counter + 1 });
+  };
   render() {
     return (
       <div>
-        <h1></h1>
-        <button onClick={this.handleClick}></button>
+        <h1>{this.state.counter}</h1>
+        <button onClick={this.handleClick}>Click Me!</button>
       </div>
     );
   }
