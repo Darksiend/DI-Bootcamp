d44dd80b24c3315552b5b32eb99cc6d6fa50f7f9
Darksiend <logosmk2@gmail.com>
2022-11-30 11:41:01 +0200

    Work

---
+++ Week8/Day4/exercises/ex1/src/Components/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MyNavBar from "./NavBar";
+
+const HomeScreen = () => {
+  return (
+    <>
+      <MyNavBar />
+      <div>HomeScreen</div>
+    </>
+  );
+};
+
+export default HomeScreen;
