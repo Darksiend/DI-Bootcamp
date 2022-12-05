97fbc016035f516633342148939f4898e700420c
Darksiend <logosmk2@gmail.com>
2022-12-05 12:41:07 +0200

    Work

--- Week9/day2/exercisexp/src/App.js
+++ Week9/day2/exercisexp/src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addTransaction } from "./store/transactionsSlice";
+import { addTransaction, deleteTransaction } from "./store/transactionsSlice";
 function App() {
   const [an, setAn] = useState("");
   const [fsc, setFsc] = useState("");
@@ -10,6 +10,7 @@ function App() {
   const [amount, setAmount] = useState("");
   const dispatch = useDispatch();
   const { list, index } = useSelector((state) => state.transactions);
+  console.log(list);
   const addTransactionToLocalStore = (obj) => {
     console.log(localStorage.getItem("transactions"));
     let transactions = JSON.parse(localStorage.getItem("transactions") || "[]");
@@ -17,6 +18,13 @@ function App() {
     transactions.push(obj);
     localStorage.setItem("transactions", JSON.stringify(transactions));
     console.log(localStorage);
+  };
+
+  const deleteFromList = () => {
+    let list = getTransactionFromLocalStorage();
+  };
+  const getTransactionFromLocalStorage = () => {
+    return JSON.parse(localStorage.getItem("transactions") || "[]");
   };
 
   useEffect(() => {
@@ -30,51 +38,82 @@ function App() {
   }, []);
   return (
     <>
-      <input
-        onChange={(event) => setAn(event.target.value)}
-        type="text"
-        name="an"
-        placeholder="Account Number"
-      />
-      <input
-        onChange={(event) => setFsc(event.target.value)}
-        type="text"
-        name="fsc"
-        placeholder="FSC"
-      />
-      <input
-        onChange={(event) => setHoldingName(event.target.value)}
-        type="text"
-        name="holdingName"
-        placeholder="A/C Holder Name"
-      />
-      <input
-        onChange={(event) => setAmount(event.target.value)}
-        type="text"
-        name="amount"
-        placeholder="Amount"
-      />
-      <button
-        onClick={() => {
-          addTransactionToLocalStore({
-            an: an,
-            fsc: fsc,
-            holdingName: holdingName,
-            amount: amount,
-          });
-          dispatch(
-            addTransaction({
+      <div className="form">
+        <input
+          onChange={(event) => setAn(event.target.value)}
+          type="text"
+          name="an"
+          placeholder="Account Number"
+        />
+        <input
+          onChange={(event) => setFsc(event.target.value)}
+          type="text"
+          name="fsc"
+          placeholder="FSC"
+        />
+        <input
+          onChange={(event) => setHoldingName(event.target.value)}
+          type="text"
+          name="holdingName"
+          placeholder="A/C Holder Name"
+        />
+        <input
+          onChange={(event) => setAmount(event.target.value)}
+          type="text"
+          name="amount"
+          placeholder="Amount"
+        />
+        <button
+          onClick={() => {
+            addTransactionToLocalStore({
               an: an,
               fsc: fsc,
               holdingName: holdingName,
               amount: amount,
-            })
-          );
-        }}
-      >
-        Add Transaction
-      </button>
-      <div className="list"></div>
+            });
+            dispatch(
+              addTransaction({
+                an: an,
+                fsc: fsc,
+                holdingName: holdingName,
+                amount: amount,
+              })
+            );
+          }}
+        >
+          Add Transaction
+        </button>
+      </div>
+      <div className="list">
+        {list.map((item, index) => (
+          <div className="item-container" key={item.payload.fsc}>
+            <table className="table" border="1">
+              <tr>
+                <th>ID</th>
+                <th>A/N</th>
+                <th>FSC</th>
+                <th>Name</th>
+                <th>Amount</th>
+              </tr>
+              <tr>
+                <td>{index + 1}</td>
+                <td>{item.payload.an}</td>
+                <td>{item.payload.fsc}</td>
+                <td>{item.payload.holdingName}</td>
+                <td>{item.payload.amount}</td>
+              </tr>
+            </table>
+            <button>Edit</button>
+            <button
+              onClick={() => {
+                dispatch(deleteTransaction(item));
+              }}
+            >
+              Delete
+            </button>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
--- Week9/day2/exercisexp/src/store/transactionsSlice.js
+++ Week9/day2/exercisexp/src/store/transactionsSlice.js
@@ -7,7 +7,11 @@ export const transactionsSlice = createSlice({
     addTransaction(state, action) {
       state = { list: state.list.push(action), index: state.index++ };
     },
+    deleteTransaction(state, action) {
+      let oldList = state.list;
+      let indexOfAction;
+    },
   },
 });
-export const { addTransaction } = transactionsSlice.actions;
+export const { addTransaction, deleteTransaction } = transactionsSlice.actions;
 export default transactionsSlice.reducer;
